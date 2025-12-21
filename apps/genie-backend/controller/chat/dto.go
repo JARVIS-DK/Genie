49940f0f1dcaafd516cc04efd5947782806835a0
Dhanush Kumar <dhanushkumar.dev.sde@gmail.com>
2025-12-22 01:49:42 +0530

    feat:  seperate image gen

--- apps/genie-backend/controller/chat/dto.go
+++ apps/genie-backend/controller/chat/dto.go
@@ -48,3 +48,12 @@ type RenameConversationRequestDto struct {
 	ConversationId string `json:"conversation_id" bson:"conversation_id"`
 	Name           string `json:"name" bson:"name"`
 }
+
+type GenerateImageDto struct {
+	Query          string   `json:"query"`
+	ImageModel     string   `json:"image_model" default:"gemini-3-pro-image-preview"`
+	ImageSize      string   `json:"image_size" default:"1K"`
+	AspectRatio    string   `json:"aspect_ratio" default:"1:1"`
+	NumberofImages any      `json:"number_of_images" default:"1"`
+	ImageUrls      []string `json:"image_urls"` // only editing image
+}
--- libs/shared/db_connectors/model/collections.go
+++ libs/shared/db_connectors/model/collections.go
@@ -1,8 +1,10 @@
 package model
 
 var CollectionName = map[string]string{
-	"USERS":                "users",
-	"CHAT_HISTORY":         "chat_history",
-	"CONVERSATION_HISTORY": "conversation_history",
-	"API_KEYS":             "api_keys",
+	"USERS":                    "users",
+	"CHAT_HISTORY":             "chat_history",
+	"CONVERSATION_HISTORY":     "conversation_history",
+	"API_KEYS":                 "api_keys",
+	"IMAGE_GENERATION_HISTORY": "image_generation_history",
+	"":                         "",
 }
